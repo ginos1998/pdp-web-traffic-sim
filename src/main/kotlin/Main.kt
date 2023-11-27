282cdf4ab7063e2694f02f1cb6d1948545ce7dc1
Agustin-Maffini <maffiniagustin@gmail.com>
2023-11-27 19:47:26 -0300

    - Comite el main

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -2,20 +2,45 @@ import models.*
 import controllers.*
 import java.util.*
 import kotlin.collections.ArrayList
+import kotlin.collections.Map
 
 fun main() {
     val routerController = RouterController()
 
-    val page = Page(1, "Hola mundo",0,1,2)
+    val routers = ArrayList<Router>()
+
+    val ip_o = IP(1,1)
+    val ip_d = IP(2,1)
+
+    val ab = 3 //2 bytes por segundo
+
+    val outBuffR12: Queue<Package> = LinkedList()
+    val outMapR1 = mutableMapOf<Int, Queue<Package>>()
+    outMapR1[2] = outBuffR12
+
+    val page = Page(1, "Hola mundo",0,ip_d,ip_o)
     page.setSize(page.getPageContent().length)
 
-    val router = Router(1,"", ArrayList(), ArrayList(),LinkedList(),LinkedList(), mutableMapOf(), mutableMapOf())
+    val router1 = Router(1,"", ArrayList(), ArrayList(),LinkedList(),LinkedList(), outMapR1, mutableMapOf())
+    val router2 = Router(2,"", ArrayList(), ArrayList(),LinkedList(),LinkedList(), mutableMapOf(), mutableMapOf())
 
-    routerController.receivePages(router, page)
+    val term1 = Terminal(1,"", LinkedList(), router1)
+    val term2 = Terminal(2,"", LinkedList(), router1)
+    val term3 = Terminal(1,"", LinkedList(), router2)
 
-    val p1 = router.getReceivedPages().poll()
+    val terminalRouter1 = ArrayList<Terminal>()
+    val terminalRouter2 = ArrayList<Terminal>()
 
-    println("Id de la pagina: ${p1.getPageId()}")
-    println("Contenido de la pagina: ${p1.getPageContent()}")
-    println("Peso de la pagina: ${p1.getSize()}")
+    terminalRouter1.add(term1)
+    terminalRouter1.add(term2)
+    terminalRouter2.add(term3)
+
+    router1.setTerminalTable(terminalRouter1)
+
+    routers.add(router1)
+    routers.add(router2)
+
+    routerController.receivePages(router1, page) //envio la pagina desde una terminal conectada al router1 hacia este ultimo
+    routerController.pageSegmentation(router1, page, ab)
+    println("Segmentado...")
 }
